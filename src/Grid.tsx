646e34c68c6fe42324bad37e84a94387c2a3f195
nickschirloff <aceartistpie@gmail.com>
2025-03-25 23:10:34 -0500

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,22 +1,57 @@
 import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
+import { ColDef, IFilterOptionDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
-const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+const convertStringToNumber = (value: string) => {
+  return Number(value);
+}
+
+const compareNumericStrings = (valA: string, valB: string) => {
+  const convertedValA = convertStringToNumber(valA);
+  const convertedValB = convertStringToNumber(valB);
+
+  if (isNaN(convertedValA) && isNaN(convertedValB)) {
+      return 0;
+  } else if (isNaN(convertedValA)) {
+      return 1;
+  } else if (isNaN(convertedValB)) {
+      return -1;
+  }
+  return convertedValA - convertedValB;
+}
+
+const numericStringColumnFilter: IFilterOptionDef[] = [
+  {
+      displayKey: "lessThanWithNulls",
+      displayName: "Less Than",
+      predicate: ([filterValue], cellValue) => Number(cellValue) === undefined || Number(cellValue) < Number(filterValue)
+  },
+  {
+      displayKey: "greaterThanWithNulls",
+      displayName: "Greater Than",
+      predicate: ([filterValue], cellValue) => Number(cellValue) === undefined || Number(cellValue) > Number(filterValue)
+  },
+  {
+      displayKey: "doesNotEqual",
+      displayName: "Does Not Equal",
+      predicate: ([filterValue], cellValue) => Number(cellValue) !== Number(filterValue)
+  },
 ];
 
+const columnDefs: ColDef[] = [
+  { field: "designation", headerName: "Designation", filter: "agTextColumnFilter" },
+  { field: "discovery_date", headerName: "Discovery Date", filter: "agSetColumnFilter" },
+  { field: "h_mag", headerName: "H (mag)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "moid_au", headerName: "MOID (au)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "q_au_1", headerName: "q (au)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "q_au_2", headerName: "Q (au)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "period_yr", headerName: "Period (yr)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "i_deg", headerName: "Inclination (deg)", comparator: compareNumericStrings, filter: "agNumberColumnFilter", filterParams: { filterOptions: numericStringColumnFilter } },
+  { field: "pha", headerName: "Potentially Hazardous", filter: "agTextColumnFilter" },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, filter: "agTextColumnFilter" },
+];
 const NeoGrid = (): JSX.Element => {
   const appTitle = "Near-Earth Object Overview";
   document.title = appTitle;
